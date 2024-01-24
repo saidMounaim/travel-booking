48f301010e7d1b34ccb914b0823f6b18f727d9fa
saidMounaim <saidmounaim00@gmail.com>
2024-01-24 10:41:24 +0100

    Add Navbar Component

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="bg-white border py-4">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex items-center justify-between">
+          <Link href="/" className="text-gray-500 font-bold text-2xl">
+            Travel Booking
+          </Link>
+          <nav className="flex flex-col">
+            <ul className="flex items-center gap-6">
+              <li className="text-md text-gray-950 font-medium">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="text-md text-gray-950 font-medium">
+                <Link href="/">About</Link>
+              </li>
+              <li className="text-md text-gray-950 font-medium">
+                <Link href="/">Tours</Link>
+              </li>
+              <li className="text-md text-gray-950 font-medium">
+                <Link href="/">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex items-center gap-6">
+            <Link href="/sign-in" className="text-md text-gray-950 font-medium">
+              Sign In
+            </Link>
+            <Link
+              href="/sign-up"
+              className="bg-blue-700 text-md font-medium py-2 px-4 text-white rounded-md"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
