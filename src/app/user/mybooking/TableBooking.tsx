d944543e5ef22acc33d4e5e15ca0d75580f6894a
saidMounaim <saidmounaim00@gmail.com>
2024-02-01 12:53:34 +0100

    Add Delete My Booking Funcionality

--- src/app/user/mybooking/TableBooking.tsx
+++ src/app/user/mybooking/TableBooking.tsx
@@ -1,6 +1,9 @@
-import { Reservation } from "@prisma/client";
+"use client";
+
 import { Trash2 } from "lucide-react";
 import Image from "next/image";
+import { deleteMyBooking } from "./action";
+import { usePathname } from "next/navigation";
 
 interface TableBookingProps {
   booking: {
@@ -8,6 +11,7 @@ interface TableBookingProps {
     name: string;
     email: string;
     mobile: string;
+    userId: number;
     tour: {
       title: string;
       feauturedImage: string;
@@ -16,57 +20,71 @@ interface TableBookingProps {
 }
 
 const TableBooking = ({ booking }: TableBookingProps) => {
+  const path = usePathname();
+
   return (
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-          <th scope="col" className="px-6 py-3">
-            Featured Image
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Tour Title
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Full Name
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Email
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Mobile
-          </th>
-          <th scope="col" className="px-6 py-3"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {booking.map((b) => (
-          <tr
-            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            key={b.id}
-          >
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              <Image
-                src={b.tour.feauturedImage}
-                alt="Featured Image"
-                width={80}
-                height={80}
-                className="object-cover rounded-md"
-              />
-            </th>
-            <td className="px-6 py-4">{b.tour.title}</td>
-            <td className="px-6 py-4">{b.name}</td>
-            <td className="px-6 py-4">{b.email}</td>
-            <td className="px-6 py-4">{b.mobile}</td>
-            <td className="px-6 py-4">
-              <Trash2 size={20} fill="transparent" stroke="#D04848" />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      {booking.length == 0 && <h1 className="text-center">No Booking Found</h1>}
+      {booking.length > 0 && (
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Featured Image
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tour Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Full Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Mobile
+              </th>
+              <th scope="col" className="px-6 py-3"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {booking.map((b) => (
+              <tr
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                key={b.id}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  <Image
+                    src={b.tour.feauturedImage}
+                    alt="Featured Image"
+                    width={80}
+                    height={80}
+                    className="object-cover rounded-md"
+                  />
+                </th>
+                <td className="px-6 py-4">{b.tour.title}</td>
+                <td className="px-6 py-4">{b.name}</td>
+                <td className="px-6 py-4">{b.email}</td>
+                <td className="px-6 py-4">{b.mobile}</td>
+                <td className="px-6 py-4">
+                  <form action={deleteMyBooking}>
+                    <input type="hidden" name="path" value={path} />
+                    <input type="hidden" name="userId" value={b.userId} />
+                    <input type="hidden" name="bookingId" value={b.id} />
+                    <button type="submit">
+                      <Trash2 size={20} fill="transparent" stroke="#D04848" />
+                    </button>
+                  </form>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
 
---
+++ src/app/user/mybooking/action.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export async function deleteMyBooking(formData: FormData) {
+  const userId = formData.get("userId");
+  const bookingId = formData.get("bookingId");
+  const path = formData.get("path");
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || session?.user.id !== Number(userId)) {
+    redirect("/");
+  }
+
+  const book = await prisma.reservation.findFirst({
+    where: { id: Number(bookingId) },
+  });
+
+  if (!book) {
+    throw new Error("Book not found");
+  }
+
+  await prisma.reservation.delete({ where: { id: book.id } });
+
+  revalidatePath(path as string);
+}
